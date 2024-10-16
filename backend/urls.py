182dd4c6cd373a7622db13314c2706fd262a27ae
nsangou610 <hello@gmail.com>
2024-10-17 00:04:37 +0100

    modified

--- backend/urls.py
+++ backend/urls.py
@@ -2,15 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-# from api.views import EvenementViewSet, RapportPerformanceViewSet, UtilisateurViewSet, EleveViewSet, EnseignantViewSet, ClasseViewSet, CoursViewSet, NoteViewSet, AbsenceViewSet,TrimestreViewSet,BulletinViewSet,FichierViewSet,PresenceViewSet
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/auth/', include('userapp.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 
